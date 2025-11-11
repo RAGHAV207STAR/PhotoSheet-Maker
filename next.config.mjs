79a4e192305a49d30cb914dd4cecc6fb30a73bd3
RAGHAV <raghavpratap987654@gmail.com>
2025-11-11 12:38:37 +0530

    F

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["6000-firebase-studio-1760423353153.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"],
-  },
   images: {
     remotePatterns: [
       {
@@ -10,6 +7,13 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
       },
     ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        '6000-firebase-studio-1760423353153.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev',
+      ],
+    },
   },
 };
 
