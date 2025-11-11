910ad6fc2c145b25e40c5e0307f9db00b347aea5
RAGHAV <raghavpratap987654@gmail.com>
2025-11-11 11:36:30 +0530

    G

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-        ],
-    },
+  experimental: {
+    allowedDevOrigins: ["6000-firebase-studio-1760423353153.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
