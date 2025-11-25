5a169486799a0bfd99c57057141dba0707a6147f
RAGHAV <raghavpratap987654@gmail.com>
2025-11-25 20:10:32 +0530

    g

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["*.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"],
-  },
   images: {
     remotePatterns: [
       {
